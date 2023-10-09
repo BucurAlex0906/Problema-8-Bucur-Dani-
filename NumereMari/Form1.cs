1804b3a4c79e8bae3adedb1ce4b69be098991cfc
s225 <s225@DESKTOP-D2KQ6PI>
2023-10-09 10:06:34 +0300

    Add project files.

---
+++ NumereMari/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumereMari
+{
+    public partial class Form1 : Form
+    {
+        int nrNoduri;
+        List<List<int>>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
